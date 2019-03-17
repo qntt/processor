046db156f49ad75da1f6f3bae7162a1334cbe735
Minh Tran <minh.tran@duke.com>
2019-03-17 17:40:44 -0400

    changes to test bench

--- proc_tb.v
+++ proc_tb.v
@@ -20,6 +20,10 @@ module proc_tb();
 	 wire[31:0] d_mw = dut.my_processor.d_mw;
 	 wire[31:0] b_out_regfile = dut.my_processor.b_out_regfile;
 	 wire sel2_mx = dut.my_processor.sel2_mx;
+	 wire MX1 = dut.my_processor.MX1;
+	 wire WX1 = dut.my_processor.WX1;
+	 wire MX2 = dut.my_processor.MX2;
+	 wire WX2 = dut.my_processor.WX2;
 	 
     initial
     begin
@@ -28,7 +32,7 @@ module proc_tb();
         
         //$monitor("clock: %d, pc: %d, a_dx: %d, o_xm: %b, isI_x: %d, signextend: %b, alu_input_2: %b", clock, pc, a_dx, o_xm, isI_x, signextend, alu_input_2);
 		  
-		  $monitor("pc: %d, b_out_regfile: %d, sel2_mx: %d, o_xm: %d, b_xm: %d, d_mw: %d", pc, b_out_regfile, sel2_mx, o_xm, b_xm, d_mw);
+		  $monitor("pc: %d, b_out_regfile: %d, sel2_mx: %d, o_xm: %d, b_xm: %d, d_mw: %d, MX1: %d, WX1: %d, MX2: %d, WX2: %d", pc, b_out_regfile, sel2_mx, o_xm, b_xm, d_mw, MX1, WX1, MX2, WX2);
 		  
 		  #(20*(CYCLE_LIMIT+1.5))
 
